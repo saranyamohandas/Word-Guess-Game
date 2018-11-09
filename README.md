dfac19380b19e08fece1d7cfd45a6dbbf354e0d3
saranyamohandas <saranyamohandas2@gmail.com>
2018-11-08 22:32:06 -0800

    Word guess game

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Word-Guess-Game
-Homework #3B-Word Guess Game
+---
+
+Homework #3B - Word Guess Game
+___
+
+### This is a interactive game application that has been developed using HTML,CSS,bootstrap and javascript 
+
+### Overview of the application -
+* This application requires two player. One player will be the computer and user will be the other player. 
+* Start page displays the rules of the game as mentioned below.
+* Computer randomly generates a word each time player clicks start button.
+* Application shows blanks and user has to guess the word by pressing letters via keyboard.
+* Application picks up the key and if the letter is available in the word, it displays at appropriate blanks.
+* Maximum 10 attempts are allowed.
+* If User guesses correctly, User wins and if User fail to guess the word before after 10 attempts he loses the game.
+
+### Overview of the script -
+* Once the player clicks 'Start' button, the game starts.
+* Counters are defined to track number of guesses and number of attempts remaining for the user.
+* Words are randomly generated using Math function.
+* Letters are captured by using dom  keyboard events.
+* Blanks are generated dynamically by javascript.
+* When correct letter is guessed, blanks are filled with letter dynamically.
+* When player wins, Winner image is displayed whereas when player loses, loser image is displayed.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,53 +7,74 @@ var guess = 0;
 var attempt = 10;
 var userInput = [];
 var getAllIdx = [];
+var alphabets = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//get user input and calculate # of attempts    
 document.onkeyup = function getUserinput(event){
-    var getKey = event.key; //get user input
-    guess++;
-    if(attempt != 0){
-        attempt--;
+    var getKey = event.key.toLowerCase();
+    for (i in alphabets) {
+         if(getKey == alphabets[i].toLowerCase()){
+             guess++;
+             if(attempt != 0){
+              attempt--;
+             }
+               
+              document.getElementById("p1").style.fontSize = '20px';
+             document.getElementById("p1").style.fontFamily = "Poor Story";
+                document.getElementById("p1").innerHTML = "Remaining guesses - " + attempt;
+                if(guess == 10){
+                  document.getElementById('rstBtn').style.display = 'block';
+                  document.getElementById('p2').style.color = 'red';
+                  document.getElementById('cardImg').src = 'assets/images/Loser_icon.png';
+                  document.getElementById('cardImg').style.height = '100'; 
+                  document.getElementById('cardImg').style.display = 'block';
+                  document.getElementById('p2').innerHTML = 'Game over! maximum guesses attempted!';
+
+                    }
+             
+                if((userInput.indexOf(getKey) == -1) && (guess != 10)){
+
+                    userInput.push(event.key);
+                    console.log(userInput);
+                    checkInput(getKey)
+                    showUserInp()
+                }
+         }
     }
-    
-    document.getElementById("p1").innerHTML = "Guess remaining - " + attempt;
-    if(guess == 10){
-      document.getElementById('p2').innerHTML = 'Game over - maximun guesses attempted';
-            
-        }
-    if((userInput.indexOf(getKey) == -1) && (guess != 10) ){
-        
-        userInput.push(event.key);
-        console.log(userInput);
-        checkInput(getKey)
-        showUserInp()
-        
-       }
-   }
+}
 function showUserInp(){
-    document.getElementById('p3').innerHTML = "User input - " + userInput;
+    document.getElementById("p3").style.fontSize = '30px';
+    document.getElementById("p3").style.fontFamily = "Poor Story";
+    document.getElementById('p3').innerHTML = "Your input - " + userInput;
 }
 function checkInput(val){
     var inputIdx = 0;
     var pos = 0;
     var match = 0;
-    
     while ((inputIdx != -1) && (inputIdx  < gameWord.length)){
         
         inputIdx = gameWord.indexOf(val,inputIdx);
          console.log('inputIdx-',inputIdx);
         if(inputIdx != -1) {
-//           
-//           console.log('inputIdx != -1',inputIdx != -1)
-              getAllIdx.push(inputIdx);
-//           
-           inputIdx++;
+            getAllIdx.push(inputIdx);     
+            inputIdx++;
          }
-//       
+      
       }
       console.log('index-',getAllIdx);
        newNode(getAllIdx);
 }
-
+//dynamically generate blanks to fill in gameword
 function blankSpacegen(blanks){
+    document.getElementById('jumbo').style.display = 'none';
+    document.getElementById('card1').style.marginTop = '50px';
+    document.getElementById('card1').style.width = '40%';
+ document.getElementById('card1').style.left = '450px';
+    document.getElementById('startBtn').style.display = 'none';
+    document.getElementById("p2").style.fontSize = '30px';
+    document.getElementById("p2").style.fontFamily = 'Poor Story';
+    
+    document.getElementById('p2').innerHTML = 'Start guessing!';
     var node = document.createElement('span');
     var text = document.createTextNode('_');
     node.appendChild(text);
@@ -67,26 +88,43 @@ function blankSpacegen(blanks){
      }
         
 }
+//check if the letter appears more than once
+// create a text node to fill the corresponding blanks
  function newNode(idx){
-    //check if the letter appears more than once
-    // create a text node to fill in the blanks
+  
     for (x in idx){
         var txtIdx = idx[x];
+        
         var newletter = document.createTextNode(gameWord.charAt(txtIdx));
         console.log('x - ',x);
         var letter = document.getElementById('id1').childNodes[txtIdx];
+        letter.style.color = '#FF530D';
+        letter.style.fontSize = '50px';
+        letter.style.fontFamily = "Black And White Picture";
+        
          console.log(letter);
         letter.replaceChild(newletter,letter.childNodes[0]);
     }
      winCheck(idx);
 }
-
+//Check if length of gameword & guessed word array are equal //to check if the player has won the game
 function winCheck(arr){
     if(arr.length == gameWord.length){
-        document.getElementById('p2').innerHTML = 'Winner!!';
+        document.getElementById('p2').style.color = 'blue';
+        document.getElementById('p2').style.textAlign = 'center';
+        document.getElementById('p2').style.fontSize = '30px'; 
+        document.getElementById('p2').innerHTML = 
+            'hooray!!!Winner!!';
+        document.getElementById('cardImg').style.display = 'block';
+        document.getElementById('rstBtn').style.display = 'block';
+        
     }
     
     
+}
+// refresh/reload the page
+function reset(){
+    location.reload();
 }
 
 
